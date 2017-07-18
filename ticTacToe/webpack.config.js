55cd16a5c820686025a29894cd72d49a995e8af8
hdorothea <dorothea.hormann@gmail.com>
2017-07-18 17:34:55 -0400

    add babel-polyfill

--- ticTacToe/webpack.config.js
+++ ticTacToe/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/main.js',
+  entry: ['babel-polyfill', './app/main.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname),
